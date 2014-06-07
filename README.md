22b2226e082cd7ed9bd02c97da94cb70e259f8b0
James Andrews <jka6502@gmail.com>
2014-06-07 18:44:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tomorrow
+
+> Mañana, mañana...
+
+Tomorrow is an experimental alternative approach to handle the asynchronous hell
+that commonly occurs in JavaScript applications.
+
+It is currently in a prototype phase, does not have any formal tests, is
+missing features, and is absolutely liable to change!
+
+Still, don't let that stop you giving it a whirl.
---
+++ test.js
@@ -0,0 +1,49 @@
+(function() {
+	"use strict";
+
+
+	// TEMPORARY TEST FILE
+
+
+	var Fiber		= require('fibers'),
+		Tomorrow	= require('./lib/tomorrow');
+
+
+
+	(Fiber(function() {
+
+		var readFile = Tomorrow.wrap(require('fs').readFile);
+
+		var timeout = Tomorrow.wrap(setTimeout, 0);
+		var madness = timeout(2000);
+
+		function delayResponse(callback) {
+			setTimeout(function() {
+				callback(null, function() { console.log('Called...'); return 'hello'; });
+			}, 1000);
+		}
+
+		var hello = Tomorrow.wrap(delayResponse)();
+
+		var result = readFile('./package.json');
+
+		console.warn("Awaiting file read...");
+		console.warn("Read: " + result);
+
+		console.log('Hello:', hello());
+
+		console.log('Timeout resolved...', Tomorrow.wait(madness), 'of course');
+
+		function getMeFiles() {
+			return readFile('non-existant.wut?');
+		}
+
+		console.log(getMeFiles());
+
+	})).run();
+
+	console.log("I can haz other paths?");
+
+
+
+})();
