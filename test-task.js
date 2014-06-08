fb633a15ad7dc43eb5f3ab0205fbb592ab6bec53
James Andrews <jka6502@gmail.com>
2014-06-08 13:28:35 +0100

    Add task.sleep Extend task example with convoluted class methods, and 143% more silliness.

--- test-task.js
+++ test-task.js
@@ -6,11 +6,52 @@
 
 		task		= require('./lib/task'),
 		method		= task.method,
-		series		= task.series;
+		series		= task.series,
+		sleep		= task.sleep;
 
 
-	var timeout = tomorrow.wrap(setTimeout, 0);
-	var sleep = function(time) { wait(timeout(time)); };
+	function TestClass() {}
+
+	TestClass.prototype = {
+
+		doTheThing: method.series(function() {
+			this.scorePoints();
+			this.grobbleFillanges();
+			console.log("We earned: " + this.calculateProfit());
+		}),
+
+		scorePoints: method.task(function() {
+			console.log("Attempting to score points");
+			sleep(2000);
+			console.log("Points scored!");
+		}),
+
+		grobbleFillanges: method.task(function() {
+			console.log("Fillanges discovered, grobbling commencing");
+			for(var count = 5; count >= 0; count--) {
+				this.faffIncessantly();
+			}
+			console.log("All fillanges currently being grobbled");
+		}),
+
+		faffIncessantly: method.task(function() {
+			console.log("Initiating faffing protocol...");
+			sleep(70);
+			console.log("Good faff, good faff.");
+		}),
+
+		calculateProfit: method.task(function() {
+			console.log("Carry the one, multiply by 'corrective' constant");
+			sleep(1000);
+			console.log("Tear up original P&L sheet, invent new form of arithmetic")
+			sleep(2000);
+			console.log("Pluck numbers from thin air");
+			sleep(1000);
+			console.log("PROFIT!");
+			return 10;
+		})
+
+	}
 
 
 	function subtask() {
@@ -61,6 +102,8 @@
 
 			console.log('Series 3 end');
 
+			var thing = new TestClass();
+			thing.doTheThing();
 		});
 
 	});
